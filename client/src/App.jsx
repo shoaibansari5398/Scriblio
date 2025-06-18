6453ff0c6c43a9e083b5ff7b4adb954aa60efeee
Shoaib Akhtar Ansari <shoaib.ansari5398@gmail.com>
2025-06-18 09:52:45 +0530

    initialise blog app

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Blog from "./pages/Blog";
+
+const App = () => {
+    return (
+		<div>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/blog" element={<Blog />} />
+			</Routes>
+        </div>
+    );
+};
+
+export default App;
