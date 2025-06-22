53065451376b08e3b0665aa8b3adb75ced97a7b5
Shoaib Akhtar Ansari <shoaib.ansari5398@gmail.com>
2025-06-22 20:05:44 +0530

    created api for blog comments

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -2,7 +2,7 @@ import imagekit from "../configs/imageKit.js";
 import fs from "fs";
 import Blog from "../models/blog.js";
 
-const addBlog = async (req, res) => {
+export const addBlog = async (req, res) => {
 	try {
 		const { title, subTitle, description, category, isPublished } = JSON.parse(
 			req.body.blog
@@ -53,4 +53,85 @@ const addBlog = async (req, res) => {
 	}
 };
 
-export default addBlog;
+export const getAllBlogs = async (req, res) => {
+	try {
+		const blogs = await Blog.find({isPublished: true});
+		res.json({success: true, blogs})
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: error.message });
+	}
+};
+
+
+export const getBlogById = async (req, res) => {
+	try {
+		const {blogId} = req.params;
+		const blog = await Blog.findById(blogId);
+		if(!blog){
+			return res.status(404).json({success: false, message: "Blog not found"})
+		}
+		res.json({success: true, blog})
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: error.message });
+	}
+}
+
+export const deleteBlogById = async (req, res) => {
+	try {
+		const {id} = req.body;
+		const blog = await Blog.findByIdAndDelete(id);
+		if(!blog){
+			return res.status(404).json({success: false, message: "Blog not found"})
+		}
+		res.json({success: true, message: "Blog deleted successfully"})
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: error.message });
+	}
+}
+
+export const togglePublish= async (req, res) => {
+	try {
+		const {id} = req.body;
+		const blog = await Blog.findById(id);
+		if(!blog){
+			return res.status(404).json({success: false, message: "Blog not found"})
+		}
+		blog.isPublished = !blog.isPublished;
+		await blog.save();
+		res.json({success: true, message: "Blog status updated"})
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: error.message });
+	}
+}
+
+export const addComment = async (req, res) => {
+
+	try {
+
+		const { blog, name, content } = req.body;
+		await Comment.create({ blog, name, content});
+		res.json({success: true, message: "Comment added for review"})
+
+
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: error.message });
+	}
+
+}
+
+
+export const getBlogComments = async (req, res) => {
+	try {
+		const {blogId} = req.body;
+		const comments = await Comment.find({blog: blogId,isApproved: true}).sort({createdAt: -1});
+		res.json({success: true, comments})
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: error.message });
+	}
+}
--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -1,10 +1,17 @@
 import express from "express";
-import addBlog from "../controllers/blogController.js";
+import { addBlog, getAllBlogs, getBlogById, togglePublish, deleteBlogById, addComment, getBlogComments } from "../controllers/blogController.js";
 import upload from "../middlewares/multer.js";
 import auth from "../middlewares/auth.js";
 
 const blogRouter = express.Router();
 
-blogRouter.post("/add",upload.single("image"), auth, addBlog);
+blogRouter.post("/add", upload.single("image"), auth, addBlog);
+blogRouter.delete("/delete", auth, deleteBlogById);
+blogRouter.post("/toggle-publish", auth, togglePublish);
+blogRouter.get("/all", getAllBlogs);
+blogRouter.get("/:blogId", getBlogById);
+blogRouter.post("/add-comment", auth, addComment);
+blogRouter.post("/comments", getBlogComments);
+
 
 export default blogRouter;
